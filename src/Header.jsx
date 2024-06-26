2210e2dd07c5ed943217c979d03648332ed258e8
Brian Le <brianle17360@gmail.com>
2024-06-25 23:46:27 -0700

    changed gradient background behavior

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,12 +1,31 @@
 import image from './assets/8bit_me_transparent.png'
+import slime from './assets/Player.png'
 import { useRef } from 'react'
 
+let count = 0;
+
+function easterEgg() {
+    if (count == 5) {
+        image = slime;
+    }
+
+    else {
+        count += 1;
+    }
+
+    console.log(count);
+}
+
 function Header(prop) {
+    let img = image;
+
     return(
         <>
             <div className="header-div">
                 <div className="name-img-div">
-                    <img src={image} alt="8bit image" height="100" className="_8bit-img"></img>
+                    <button style={{background: "none", border: "none", }} onClick={() => {count == 5 ? img = slime : count += 1}}>
+                        <img src={img} alt="8bit image" height="100" className="_8bit-img"></img>
+                    </button>
                     <p className="title">{prop.name}</p>
                 </div>
 
