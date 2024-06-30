80809f215b8f3de2d968a19d00b39ee49cba7aeb
Brian Le <brianle17360@gmail.com>
2024-06-30 00:48:11 -0700

    fixed header over-scaling. gave max-width

--- src/Header.jsx
+++ src/Header.jsx
@@ -2,6 +2,19 @@ import image from './assets/8bit_me_transparent.png'
 import slime from './assets/Player.png'
 import { useRef } from 'react'
 
+/*
+                <div className="name-img-div">
+                    <button style={{background: "none", border: "none", }}>
+                        <img src={img} alt="8bit image" height="100" className="_8bit-img"></img>
+                    </button>
+
+                    <button className="home-button" style={{cursor: "pointer"}} onClick={() => {
+                            prop.introRef.current.scrollIntoView({behavior: 'smooth', block: 'end'})
+                    }}>{prop.name}</button>
+
+                </div>
+                */
+
 function Header(prop) {
     let img = image;
 
@@ -21,21 +34,21 @@ function Header(prop) {
 
                 <nav className="navbar">
                     <ul>
-                        <button style={{cursor: "pointer"}} onClick={() => {
-                            prop.aboutRef.current.scrollIntoView({behavior: 'smooth'})
-                        }}>About</button>
+                            <button style={{cursor: "pointer"}} onClick={() => {
+                                prop.aboutRef.current.scrollIntoView({behavior: 'smooth'})
+                            }}>About</button>
 
-                        <button style={{cursor: "pointer"}} onClick={() => {
-                            prop.skillsRef.current.scrollIntoView({behavior: 'smooth'})
-                        }}>Skills</button>
+                            <button style={{cursor: "pointer"}} onClick={() => {
+                                prop.skillsRef.current.scrollIntoView({behavior: 'smooth'})
+                            }}>Skills</button>
 
-                        <button style={{cursor: "pointer"}} onClick={() => {
-                            prop.projRef.current.scrollIntoView({behavior: 'smooth'})
-                        }}>Projects</button>
-                        
-                        <button style={{cursor: "pointer"}} onClick={() => {
-                            prop.contactRef.current.scrollIntoView({behavior: 'smooth'})
-                        }}>Contact</button>
+                            <button style={{cursor: "pointer"}} onClick={() => {
+                                prop.projRef.current.scrollIntoView({behavior: 'smooth'})
+                            }}>Projects</button>
+                            
+                            <button style={{cursor: "pointer"}} onClick={() => {
+                                prop.contactRef.current.scrollIntoView({behavior: 'smooth'})
+                            }}>Contact</button>
                     </ul>
                 </nav>
             </div>
