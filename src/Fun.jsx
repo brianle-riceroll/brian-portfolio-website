0f2beb2839bd091fcb9c85b8fcd4098fd1e5b489
Brian Le <brianle17360@gmail.com>
2024-06-30 12:51:32 -0700

    made easter egg harder to find

--- src/Fun.jsx
+++ src/Fun.jsx
@@ -1,7 +1,7 @@
 function Fun() {
     return(
         <div>
-            <p className="fun-div" style={{fontSize: "0.3em"}}>Did you find the easter egg? (Hint: Click on "me" 5 times!)</p>
+            <p className="fun-div" style={{fontSize: "0.5em"}}>Did you find the easter egg? (Hint: Click on "me" 5 times!)</p>
         </div>
     );
 }
