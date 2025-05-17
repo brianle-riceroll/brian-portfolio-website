e17867faf0de06f112153c79879858e845763949
Brian Le <brianle17360@gmail.com>
2025-05-17 12:35:44 -0700

    updated resume, and discord ai bot project, and removed old stale skills

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -14,8 +14,7 @@ import discord_bot from '../assets/discordbot.gif'
 import discord_ai from '../assets/discord_ai.gif'
 import sss_img from '../assets/sss_img.gif'
 import kioku from '../assets/kioku_banner.png'
-
-
+import miko from '../assets/miko_banner_art.gif'
 
 function Home() {
   const aboutRef = useRef(null);
@@ -53,11 +52,10 @@ function Home() {
 
       <h1 className="section-header" ref={projRef}>Projects</h1>
 
-      <Project title="Discord AI Chatbot" 
-        description="Developing a Discord AI Chatbot, responding to user comments with the ability to imitate any character from your favorite TV shows, movies, and more. Leveraging the ChatGPT-4 API for generating responses."
-        img={discord_ai} 
-        date="Dec. 2024 - Present"
-        link='https://github.com/brianle-riceroll/discord_ai_chatbot'></Project>
+      <Project title="Miko: The Awesome Discord AI Chatbot" 
+        description="This is Miko. Your witty, smart, and uniquely personable Discord AI Chatbot that interacts with users in both text and voice channels in Discord. Powered by a personally fine-tuned LlaMa 3-8B, Text-To-Speech, and Retrieval-based Voice Conversion model, all running locally. It also has the ability to play Minecraft!"
+        img={miko} 
+        date="Dec. 2024 - Present"></Project>
 
       <Project title="Kioku! The Online Flashcard" 
         description="Creating an online full-stack web app for users to create and manage flashcards for studying. Built using React with Bootstrap for the frontend, and Spring Boot with MongoDB for the backend. Hosted on Firebase and backend deployed on AWS."
